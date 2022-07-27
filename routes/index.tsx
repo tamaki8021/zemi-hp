266f30ab81e13bf0b95b1684a8947f3261fc6e89
tamaki8021 <hbk2002320@gmail.com>
2022-07-27 23:40:49 +0900

    add: Lemon Drop animation

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,21 +1,21 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
+import LemonDrop from "../islands/LemonDrop.tsx";
+import { Fotter } from "../components/Fotter.tsx";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
-    </div>
+    <html class="dark">
+      <div class={tw`w-full h-screen dark:bg-black`}>
+        <div class={tw`p-1 mx-auto max-w-screen-md `}>
+          <section>
+            <LemonDrop />
+          </section>
+          <p class={tw`my-6 dark:text-white`}>Welcome to mypage</p>
+        </div>
+        <Fotter />
+      </div>
+    </html>
   );
 }
