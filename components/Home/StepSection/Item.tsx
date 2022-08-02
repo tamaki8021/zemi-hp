83ef6dc09f02681cdae924ac6dfb54ff69652f1b
tamaki8021 <hbk2002320@gmail.com>
2022-08-02 12:05:05 +0900

    feat: stepの内容を更新

--- components/Home/StepSection/Item.tsx
+++ components/Home/StepSection/Item.tsx
@@ -2,40 +2,47 @@
 import { h, FunctionComponent } from "preact";
 import { tw } from "@twind";
 
-export const Item: FunctionComponent = () => {
+type Props = {
+  svgPath: string;
+  title: string;
+  contents: string;
+};
+
+export const Item: FunctionComponent<Props> = ({
+  svgPath,
+  title,
+  contents,
+}) => {
   return (
     <div
-      class={tw`flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl dark:bg-gray-800`}
+      class={tw`flex flex-col items-center px-6 md:px-8 py-12 space-y-3 text-center rounded-xl border border-gray-300`}
     >
       <span
-        class={tw`inline-block p-3 text-blue-500 bg-blue-100 rounded-full dark:text-white dark:bg-blue-500`}
+        class={tw`inline-block p-3 text-blue-500 rounded-full dark:text-white bg-indigo-500`}
       >
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class={tw`w-6 h-6`}
+          class={tw`h-10 w-10`}
           fill="none"
-          viewBox="0 0 24 24"
           stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
         >
           <path
             stroke-linecap="round"
             stroke-linejoin="round"
             stroke-width="2"
-            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
-          />
+            d={svgPath}
+          ></path>
         </svg>
       </span>
 
       <h1
         class={tw`text-2xl font-semibold text-gray-700 capitalize dark:text-white`}
       >
-        Copy & paste components
+        {title}
       </h1>
 
-      <p class={tw`text-gray-500 dark:text-gray-300`}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab
-        nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
-      </p>
+      <p class={tw`text-gray-500 dark:text-gray-300`}>{contents}</p>
     </div>
   );
 };
