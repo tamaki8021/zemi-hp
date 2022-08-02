21f33007cec1f5812676acd3d64bfb43f833abfd
tamaki8021 <hbk2002320@gmail.com>
2022-08-02 14:10:11 +0900

    feat: comming soonを表示

--- components/pages/Active.tsx
+++ components/pages/Active.tsx
@@ -1,10 +1,19 @@
 /** @jsx h */
 import { h, FunctionComponent } from "preact";
+import {tw} from '@twind'
+import Particles from "../../islands/Particles.tsx";
 
 export const Active: FunctionComponent = () => {
   return (
-    <section>
-      <p>active</p>
-    </section>
+    <div
+      class={tw`w-full bg-black h-screen flex flex-col justify-center items-center`}
+    >
+      <div class={tw`pt-24 p-8`}>
+        <section class={tw`max-w-3xl text-white `}>
+          <Particles />
+          <p class={tw`text-4xl md:text-9xl font-extrabold`}>COMMING SOON</p>
+        </section>
+      </div>
+    </div>
   );
 };
