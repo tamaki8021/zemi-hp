331fc119935d8375724f711e8f41d29a537f1b18
tamaki8021 <hbk2002320@gmail.com>
2022-08-02 13:06:56 +0900

    feat: professor section

--- components/Home/ProfessorSection.tsx
+++ components/Home/ProfessorSection.tsx
@@ -8,40 +8,44 @@ export const ProfessorSection: FunctionComponent = () => {
     <section class={tw`w-full mx-auto relative bg-black`}>
       <div class={tw`px-8 py-16 lg:p-24`}>
         <SectionTitle title={"PROFESSOR"} />
-        <div class={tw`items-center lg:flex`}>
-          <div class={tw`mt-8 xl:mt-12 lg:w-1/2`}>
-            <div class={tw`flex items-center justify-center`}>
-              <div class="max-w-xl">
+        <div class={tw`items-center lg:flex mt-8 md:mt-12`}>
+          <div class={tw`lg:w-1/2`}>
+            <div class={tw`flex items-center justify-center md:justify-end`}>
+              <div class={tw`max-w-xl`}>
                 <img
-                  class={tw`object-cover object-center w-full h-64 rounded-md shadow`}
-                  src={
-                    "https://images.unsplash.com/photo-1484399172022-72a90b12e3c1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-                  }
-                  alt=""
+                  class={tw`object-cover object-center w-full sm:h-72 max-h-72 rounded-md shadow`}
+                  src={"/no_image.jpg"}
+                  alt="professorの画像"
                 />
               </div>
             </div>
           </div>
-          <div class={tw`lg:w-1/2`}>
-            <h2 class={tw`text-3xl font-bold text-gray-800 dark:text-gray-100`}>
-              Who I am
-            </h2>
-
-            <p class={tw`mt-4 text-gray-500 dark:text-gray-400 lg:max-w-xl`}>
-              Hi I am jane , software engineer
-              <a
-                class={tw`font-bold text-blue-600 dark:text-blue-400`}
-                href="#"
+          <div class={tw`lg:w-1/2 mt-8 lg:mt-0 md:ml-16 items-center mx-auto`}>
+            <div class={tw`items-center text-gray-500 flex`}>
+              <h2
+                class={tw`text-3xl font-extrabold text-gray-800 dark:text-gray-100`}
               >
-                @BakaTeam
-              </a>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum in
-              sed non alias, fugiat, commodi nemo ut fugit corrupti dolorem
-              sequi ex veniam consequuntur id, maiores beatae ipsa omnis
-              aliquam?
+                赤木 徳顕
+              </h2>
+              <p class={tw`ml-6`}>AKAGI Tokuaki</p>
+            </div>
+            <p class={tw`mt-4 text-gray-500 dark:text-gray-400 lg:max-w-xl`}>
+              帝京大学 経済学部 経営学科
             </p>
+            <div class={tw`text-white pt-6`}>
+              <p class={tw`text-lg font-bold pb-2`}>- 専門分野 -</p>
+              <p>マーケティング・データサイエンス</p>
+            </div>
+            <div class={tw`inline-flex w-full sm:w-auto sm:mx-2 mt-3`}>
+              <a
+                href=""
+                class={tw`inline-flex items-center justify-center px-5 py-2 text-white bg-indigo-500 rounded-lg hover:bg-indigo-400 focus:ring focus:ring-blue-300 focus:ring-opacity-80 animate-pulse`}
+              >
+                more →
+              </a>
+            </div>
           </div>
-        </div>{" "}
+        </div>
       </div>
     </section>
   );
