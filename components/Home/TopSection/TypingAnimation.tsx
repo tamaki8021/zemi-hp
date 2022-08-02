5e705e21a323846c4a522028776bb4c310c13920
tamaki8021 <hbk2002320@gmail.com>
2022-08-02 11:17:55 +0900

    modify: topsectiolnの文章やUIなどの変更

--- components/Home/TopSection/TypingAnimation.tsx
+++ components/Home/TopSection/TypingAnimation.tsx
@@ -3,18 +3,18 @@ import { h, FunctionComponent } from "preact";
 import { tw } from "@twind";
 import { css, keyframes, animation } from "twind/css";
 
-const typeAnimation = animation("2.7s ease-out .8s infinite alternate both", {
+const typeAnimation = animation("3s ease-out .10s infinite alternate both", {
   "0%": { transform: "translateX(0ch)" },
   "5%, 10%": { transform: "translateX(1ch)" },
-  "15%, 20%": { transform: "translateX(2ch)" },
-  "25%, 30%": { transform: "translateX(3ch)" },
-  "35%, 40%": { transform: "translateX(4ch)" },
-  "45%, 50%": { transform: "translateX(5ch)" },
-  "55%, 60%": { transform: "translateX(6ch)" },
-  "65%, 70%": { transform: "translateX(7ch)" },
-  "75%, 80%": { transform: "translateX(8ch)" },
-  "85%, 90%": { transform: "translateX(9ch)" },
-  "95%, 100%": { transform: "translateX(11ch)" },
+  "15%, 20%": { transform: "translateX(3ch)" },
+  "25%, 30%": { transform: "translateX(5ch)" },
+  "35%, 40%": { transform: "translateX(8ch)" },
+  "45%, 50%": { transform: "translateX(12ch)" },
+  "55%, 60%": { transform: "translateX(16ch)" },
+  "65%, 70%": { transform: "translateX(20ch)" },
+  "75%, 80%": { transform: "translateX(22ch)" },
+  "85%, 90%": { transform: "translateX(25ch)" },
+  "95%, 100%": { transform: "translateX(28ch)" },
 });
 
 const cursorKeyframes = keyframes({
@@ -38,11 +38,12 @@ const afterStyles = css({
 
 export const TypingAnimation: FunctionComponent = () => {
   return (
-    <h1 class={tw`mb-2 text-lg text-gray-100`}>
-      hi, I&apos;m <br class={tw`block md:hidden`} />
+    <h1 class={tw`my-16 sm:my-20 text-xl text-gray-100`}>
+      帝京大学でモヤモヤしている人
+      <br class={tw`block md:hidden`} />
       <span class={tw`relative`}>
-        <span class={tw`h-20 pt-2 overflow-x-hidden whitespace-nowrap`}>
-          vanntile <span class={tw`text-lg`}>👋</span>
+        <span class={tw`h-20 pt-2 overflow-x-hidden whitespace-nowrap animate-pulse`}>
+          ステキな「<span class={tw`font-bold text-red-400`}>変態</span>」を目指しませんか？
         </span>
         <span
           class={tw`absolute -bottom-0 left-0 -top-1 inline-block bg-black w-full ${typeAnimation} ${afterStyles}`}
--- components/Home/TopSection/index.tsx
+++ components/Home/TopSection/index.tsx
@@ -7,20 +7,16 @@ import Particle from "../../../islands/Particles.tsx";
 export const TopSection: FunctionComponent = () => {
   return (
     <div
-      class={tw`text-center w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20`}
+      class={tw`text-center w-full mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8`}
     >
       <Particle />
       <h2
-        class={tw`text-3xl font-extrabold text-black dark:text-white sm:text-4xl`}
+        class={tw`text-3xl font-extrabold text-black text-gray-100 sm:text-5xl hover:animate-ping mt-6`}
       >
         <span class={tw`block`}>帝京大学 経済学部</span>
         <span class={tw`block`}>赤木ゼミナール</span>
       </h2>
-      <div class={tw`lg:mt-0 lg:flex-shrink-0`}>
-        <div class={tw`mt-6`}>
-          <TypingAnimation />
-        </div>
-      </div>
+      <TypingAnimation />
     </div>
   );
 };
