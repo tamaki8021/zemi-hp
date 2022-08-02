c93c8451ed84c470304f3661cbd4d9592d3059a6
tamaki8021 <hbk2002320@gmail.com>
2022-08-02 13:30:40 +0900

    feat: Active page

---
+++ components/pages/Active.tsx
@@ -0,0 +1,10 @@
+/** @jsx h */
+import { h, FunctionComponent } from "preact";
+
+export const Active: FunctionComponent = () => {
+  return (
+    <section>
+      <p>active</p>
+    </section>
+  );
+};
--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,8 +4,9 @@
 
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/about.tsx";
-import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
+import * as $2 from "./routes/active.tsx";
+import * as $3 from "./routes/api/joke.ts";
+import * as $4 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/Header.tsx";
 import * as $$2 from "./islands/Particles.tsx";
@@ -14,8 +15,9 @@ const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/about.tsx": $1,
-    "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/active.tsx": $2,
+    "./routes/api/joke.ts": $3,
+    "./routes/index.tsx": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
