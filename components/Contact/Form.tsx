42b0d6dd03885a116e686ef8857140ea45558d4f
tamaki8021 <hbk2002320@gmail.com>
2022-08-07 00:55:36 +0900

    modify: contactページのUIを変更

--- components/Contact/Form.tsx
+++ components/Contact/Form.tsx
@@ -4,54 +4,50 @@ import { tw } from "@twind";
 
 export const Form: FunctionComponent = () => {
   return (
-    <form
-      class={tw`flex w-full max-w-3xl space-x-6 justify-center mx-auto px-6`}
-    >
+    <form class={tw`w-full max-w-xl mx-auto px-6`}>
       <div>
-        <div
-          class={tw`mb-6 text-3xl font-light text-center text-gray-800 dark:text-white`}
-        >
-          Contact us !
-        </div>
         <div class={tw`grid max-w-xl grid-cols-2 gap-4 m-auto`}>
           <div class={tw`col-span-2 lg:col-span-1`}>
-            <div class={tw` relative `}>
+            <div class={tw`relative text-gray-200`}>
+              <label for="name">お名前</label>
               <input
                 type="text"
-                id="contact-form-name"
+                id="name"
                 class={tw` rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent`}
                 placeholder="Name"
               />
             </div>
           </div>
           <div class={tw`col-span-2 lg:col-span-1`}>
-            <div class={tw` relative `}>
+            <div class={tw`relative text-gray-200`}>
+              <label for="email">メールアドレス</label>
               <input
                 type="text"
-                id="contact-form-email"
+                id="email"
                 class={tw` rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent`}
                 placeholder="email"
               />
             </div>
           </div>
           <div class={tw`col-span-2`}>
-            <label class={tw`text-gray-700`} for="name">
-              <textarea
-                class={tw`flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent`}
-                id="comment"
-                placeholder="Enter your comment"
-                name="comment"
-                rows={5}
-                cols={40}
-              ></textarea>
+            <label class={tw`text-gray-200`} for="name">
+              お問合せ内容
             </label>
+            <textarea
+              class={tw`flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent`}
+              id="comment"
+              placeholder="Enter your comment"
+              name="comment"
+              rows={5}
+              cols={40}
+            ></textarea>
           </div>
-          <div class={tw`col-span-2 text-right`}>
+          <div class={tw`col-span-2 text-right mt-4`}>
             <button
               type="submit"
-              class={tw`py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg`}
+              class={tw`py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-full`}
             >
-              Send
+              送信
             </button>
           </div>
         </div>
