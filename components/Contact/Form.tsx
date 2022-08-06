1a50923d3b0e5212d243ff2bdd5667a68850f7d2
tamaki8021 <hbk2002320@gmail.com>
2022-08-07 01:24:09 +0900

    feat: google formとの繋げ込みの実装

--- components/Contact/Form.tsx
+++ components/Contact/Form.tsx
@@ -4,7 +4,10 @@ import { tw } from "@twind";
 
 export const Form: FunctionComponent = () => {
   return (
-    <form class={tw`w-full max-w-xl mx-auto px-6`}>
+    <form
+      class={tw`w-full max-w-xl mx-auto px-6`}
+      action="https://docs.google.com/forms/u/3/d/e/1FAIpQLSfOMUu6n3hnfs5xcKM4_rx-TlzxMjrib5rExPYCEkYk7ECwgg/formResponse"
+    >
       <div>
         <div class={tw`grid max-w-xl grid-cols-2 gap-4 m-auto`}>
           <div class={tw`col-span-2 lg:col-span-1`}>
@@ -13,7 +16,8 @@ export const Form: FunctionComponent = () => {
               <input
                 type="text"
                 id="name"
-                class={tw` rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent`}
+                name="entry.527181026"
+                class={tw`rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent`}
                 placeholder="Name"
               />
             </div>
@@ -22,22 +26,23 @@ export const Form: FunctionComponent = () => {
             <div class={tw`relative text-gray-200`}>
               <label for="email">メールアドレス</label>
               <input
-                type="text"
+                type="email"
                 id="email"
-                class={tw` rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent`}
+                name="entry.1133397221"
+                class={tw`rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent`}
                 placeholder="email"
               />
             </div>
           </div>
           <div class={tw`col-span-2`}>
-            <label class={tw`text-gray-200`} for="name">
+            <label class={tw`text-gray-200`} for="comment">
               お問合せ内容
             </label>
             <textarea
               class={tw`flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent`}
               id="comment"
               placeholder="Enter your comment"
-              name="comment"
+              name="entry.466288851"
               rows={5}
               cols={40}
             ></textarea>
