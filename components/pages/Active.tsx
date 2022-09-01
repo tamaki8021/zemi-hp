cf0fcd9df7f024a5bfbd94dffa5f5d19e3560f9e
tamaki8021 <hbk2002320@gmail.com>
2022-09-02 02:36:30 +0900

    feat: card layoutで作成

--- components/pages/Active.tsx
+++ components/pages/Active.tsx
@@ -3,13 +3,21 @@ import { h, FunctionComponent } from "preact";
 import { tw } from "@twind";
 import { TopSection } from "../common/index.ts";
 import type { Article } from "../../routes/active.tsx";
+import { ActiveCard } from "../Active/ActiveCard.tsx";
 
 type Props = {
-  article: Article[] | undefined;
+  articles: Article[] | undefined;
 };
 
-export const Active: FunctionComponent<Props> = ({ article }) => {
-  console.log(article);
+export const Active: FunctionComponent<Props> = ({ articles }) => {
+  const formatDate = (createdAt: string) => {
+    const d = new Date(createdAt);
+    const year = d.getFullYear().toString();
+    const month = d.getMonth().toString();
+    const date = d.getDate().toString();
+
+    return `${year}-${month}-${date}`;
+  };
 
   return (
     <div class={tw`w-full mx-auto bg-black pt-10`}>
@@ -18,11 +26,22 @@ export const Active: FunctionComponent<Props> = ({ article }) => {
         class={tw`w-full h-screen bg-black flex flex-col justify-center items-center bg-black relative`}
       >
         <div class={tw`pt-24 p-8`}>
-          <section class={tw`max-w-3xl text-white `}>
-            {!article ? (
-              <p class={tw`text-4xl md:text-9xl font-extrabold`}>コンテンツがありません。</p>
+          <section class={tw`text-white `}>
+            {!articles ? (
+              <p class={tw`text-4xl md:text-9xl font-extrabold`}>
+                コンテンツがありません。
+              </p>
             ) : (
-              <p class={tw`text-4xl md:text-9xl font-extrabold`}>コンテンツがあります。</p>
+              <div class="flex flex-wrap -m-4">
+                {articles.map((article) => (
+                  <ActiveCard
+                    key={article.id}
+                    title={article.title}
+                    createdAt={formatDate(article.createdAt)}
+                    imageUrl={article.imageUrl}
+                  />
+                ))}
+              </div>
             )}
           </section>
         </div>
